2e352414accd1015bae2c4985653ed0ad4222134
huynhthanhtuyen456 <huynhthanhtuyen456@gmail.com>
2024-10-24 12:33:19 +0700

    Add Lab Week7

---
+++ Week7/todoListApi/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+app = express();
+port = process.env.PORT || 3000;
+mongoose = require('mongoose');
+Task = require('./api/models/todoListModel'); // create model loading here
+const bodyParser = require('body-parser');
+
+// mongoose instance connection url
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://tuyenhtgcs220307:t0Qb4jYf8h4Y9cJQ@cluster0.jrgxr.mongodb.net/todoList');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = require('./api/routes/todoListRoutes'); // importing routes
+routes(app); // register the route
+
+app.listen(port);
+console.log("todo list RESTful API server started on: " + port);
